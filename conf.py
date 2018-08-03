2ccda89550cbf086506e499c1d01eb963306b406
Curtis Badke <curtis.badke@devfacto.com>
2018-08-03 06:38:25 -0600

    Swithc markdown compilation to use common markdown

--- conf.py
+++ conf.py
@@ -264,21 +264,7 @@ DATE_FORMAT = '%Y-%m-%d'
 # 'html' assumes the file is HTML and just copies it
 COMPILERS = {
     "rest": ('.rst', '.txt'),
-    "markdown": ('.md', '.mdown', '.markdown'),
-    "textile": ('.textile',),
-    "txt2tags": ('.t2t',),
-    "bbcode": ('.bb',),
-    "wiki": ('.wiki',),
-    "ipynb": ('.ipynb',),
-    "html": ('.html', '.htm'),
-    # PHP files are rendered the usual way (i.e. with the full templates).
-    # The resulting files have .php extensions, making it possible to run
-    # them without reconfiguring your server to recognize them.
-    "php": ('.php',),
-    # Pandoc detects the input from the source filename
-    # but is disabled by default as it would conflict
-    # with many of the others.
-    # "pandoc": ('.rst', '.md', '.txt'),
+    "commonmark": ('.md', '.mdown', '.markdown'),
 }
 
 # Create by default posts in one file format?
